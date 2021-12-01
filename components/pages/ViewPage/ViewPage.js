7d4d24fd4ecbe3af7f5a131187b41c02092f82a2
pin-cup <user@mailfence.com>
2021-12-02 01:20:09 +0300

    base

---
+++ components/pages/ViewPage/ViewPage.js
@@ -0,0 +1,29 @@
+class ViewPage extends HTMLEl {
+	get css() { return `
+			:host {
+
+			}
+	`}
+
+	get html() { return `
+
+	`}
+
+	get getEls() { return [
+	]}
+
+	static get observedAttributes() { return [
+	]}
+
+	get props() { return {
+	}}
+
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+	}
+}
+
+customElements.define(ViewPage.nameIs, ViewPage);
