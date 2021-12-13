b91f89a181e494ecbc24cccaad0ba0f7c5b5f6f8
pin-cup <user@mailfence.com>
2021-12-13 23:49:21 +0300

    simple table ++

--- components/temp/SimpleTable.js
+++ components/temp/SimpleTable.js
@@ -3,8 +3,8 @@ import('/components/temp/ReusableInput.js');
 class SimpleTable extends HTMLEl {
 	get css() { return `
 			:host {
-				height: 12rem;
-				width: 20rem;
+				height: 15rem;
+				width: 25rem;
 				box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 				padding: 1rem;
 
@@ -17,19 +17,22 @@ class SimpleTable extends HTMLEl {
 			}
 
 			.table-name {
-				--font-s: 1.5rem;
-				height: 2.5rem;
+				--font-s: 1.2rem;
+				height: 2rem;
+				width: 40%;
 			}
 
 			.table-datetime {
+				font-size: 60%;
 				color: var(--color-text-alt)
 			}
 
-			.table-datetime:hover {
+			.table-datetime:active {
 				color: var(--color-text-main)
 			}
 
 			.table-description {
+				width: 60%;
 				height: 100%;
 			}
 
@@ -40,23 +43,32 @@ class SimpleTable extends HTMLEl {
 	`}
 
 	get html() { return `
-			<reusable-input class="table-name" value="Emails"></reusable-input>
+			<reusable-input class="table-name"></reusable-input>
 
 			<div class="table-datetime">
 				<div class="table-created">25.05.21 23:00</div>
 				<div class="table-updated">13.10.21 11:23</div>
 			</div>
 
-			<reusable-input class="table-description" value="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type."></reusable-input>
+			<reusable-input class="table-description"></reusable-input>
 	`}
 
 	get getEls() { return [
-	]}
-
-	static get observedAttributes() { return [
+		'.table-name',
+		'.table-created',
+		'.table-updated',
+		'.table-description',
 	]}
 
 	get props() { return {
+		id: { observer: ({ id }) => console.info(id) },
+		name: { observer: ({ name }) => this.tableNameEl.value = name },
+		created: { observer: ({ created }) => this.tableCreatedEl.innerText = created },
+		updated: { observer: ({ updated }) => this.tableUpdatedEl.innerText = updated },
+		description: { observer: ({ description }) => this.tableDescriptionEl.value = description },
+		// options: ({ options }) => {
+		// 	const { name, created, updated, description } = options;
+		// }
 	}}
 
 	constructor() {
