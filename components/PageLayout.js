aa1792972952cbd91b407081e69f35df6a049f75
pin-cup <user@mailfence.com>
2021-12-20 20:53:23 +0300

    Add styles

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,24 +1,43 @@
 import('/components/HeaderMenu.js');
+import('/components/FooterMenu.js');
 
 class PageLayout extends HTMLEl {
 	get html() { return `
-		<header-menu></header-menu>
+		<header-menu class="header"></header-menu>
 		<div class="main childrens"></div>
-		<footer-menu></footer-menu>
+		<footer-menu class="footer"></footer-menu>
 	`}
 
 	get css() { return `
 		:host {
+			margin: auto;
 			display: flex;
 			flex-direction: column;
-			justify-content: flex-start;
+			justify-content: space-between;
 			align-items: center;
 			gap: 2rem;
+			width: 45rem;
+			min-height: 100vh;
+		}
+
+		.header {
+			width: 100%;
+			height: 5rem;
+		}
+
+		.main {
+			width: 100%;
+			min-height: calc(100vh - 14rem);
+		}
+
+		.footer {
+			width: 100%;
+			height: 5rem;
 		}
 	`}
 
 	get props() { return {
-		childrens: { observer: ({ childrens }) => childrens.forEach(children => this.childrensEl.append(children))},
+		childrens: { observer: ({ childrens }) => childrens.forEach(children => this.mainEl.append(children)) },
 	}}
 
 	connectedCallback() {
@@ -26,7 +45,9 @@ class PageLayout extends HTMLEl {
 	}
 
 	get getEls() { return [
-		'.childrens'
+		'.header',
+		'.main',
+		'.footer'
 	]}
 }
 
