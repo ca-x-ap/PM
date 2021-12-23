22516769e06a77ba1984bc9cc365d03d02b21b48
pin-cup <user@mailfence.com>
2021-12-23 23:50:49 +0300

    Add hanlders

--- components/HeaderMenu.js
+++ components/HeaderMenu.js
@@ -1,3 +1,6 @@
+import { onMenuFileUpload } from '/app/handlers.js';
+import { onMenuFileDownload } from '/app/handlers.js';
+
 class HeaderMenu extends HTMLEl {
 	get html() { return `
 		<div class="item" id="create">
@@ -5,7 +8,14 @@ class HeaderMenu extends HTMLEl {
 		</div>
 
 		<div class="item" id="dropdown">
-			<span class="item__text">Upload</span>
+			<label class="item__text">
+				Upload
+				<input class="dropdown_input" type="file" accept="application/JSON, .txt" multiple/>
+			</label>
+		</div>
+
+		<div class="item" id="download">
+			<span class="item__text">Download</span>
 		</div>
 
 		<div class="item" id="tables">
@@ -18,43 +28,38 @@ class HeaderMenu extends HTMLEl {
 			display: flex;
 			align-items: center;
 			justify-content: space-between;
+			gap: 1rem;
 			height: auto;
 			width: 100%;
-			gap: 1rem;
 			padding: 1rem;
 			background-color: var(--color-bg_accent);
 			box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 		}
 
 		.item {
-			font-family: "Montserrat-Regular";
-			display: flex;
-			justify-content: center;
 			align-items: center;
 			cursor: pointer;
+			display: flex;
+			font-family: "Montserrat-Regular";
+			justify-content: center;
 			padding: .5rem;
+			user-select: none;
+		}
+
+		.dropdown_input {
+			display: none;
 		}
 	`}
 
-	get props() { return {
-	}}
-
 	constructor() {
 		super();
-
-		this.itemEls.forEach(itemEl => {
-
-		});
-	}
-
-	connectedCallback() {
+		this.dropdownInputEl.addEventListener('change', onMenuFileUpload);
+		this.downloadEl.addEventListener('click', onMenuFileDownload);
 	}
 
 	get getEls() { return [
-		['.item'],
-	]}
-
-	static get observedAttributes() { return [
+		'.dropdown_input',
+		'#download'
 	]}
 }
 
