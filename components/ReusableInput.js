da83ad65081062dcd7fe0b0878b7e43580bfb310
pin-cup <user@mailfence.com>
2021-12-27 02:17:52 +0300

    Add reusable "input"

---
+++ components/ReusableInput.js
@@ -0,0 +1,76 @@
+class ReusableInput extends HTMLEl {
+	get html() { return `
+		<textarea class="value" spellcheck="false"></textarea>
+	`}
+
+	get css() { return `
+		:host {
+			display: flex;
+			height: 1.8rem;
+			width: 8rem;
+			--font-s: 1rem;
+			--over-v: hidden;
+
+			--font-f: 'Montserrat-Light';
+
+			--focus-background: var(--color-bg-accent);
+			--focus-color: var(--color-text-accent);
+
+			--unfocus-background: white;
+			--unfocus-color: black;
+
+			--disable-background: gray;
+			--disable-color: gray;
+		}
+
+		.value {
+			font-size: var(--font-s);
+			font-family: var(--font-f);
+			display: block;
+			resize: none;
+			overflow: var(--over-v);
+			border: 2px solid var(--color-text-alt);
+			outline: none;
+		}
+
+		.value:focus {
+			border: 2px solid var(--color-bg-alt);
+		}
+	`}
+
+	get props() { return {
+		type: { default: 'any' },
+		hidden: { observer: ({ hidden }) => setHideValueState(hidden) },
+		value: { observer: ({ value }) => this.valueEl.value = value },
+		isDisabled: { observer: ({ isDisabled }) => this.setDisabledState(isDisabled) }
+	}}
+
+	constructor() {
+		super();
+		this.valueEl.addEventListener('keypress', e => this.value = this.valueEl.value);
+	}
+
+	connectedCallback() {}
+
+	get getEls() { return [
+		'.value'
+	]}
+
+	static get observedAttributes() { return [
+		'type',
+		'hidden',
+		'isDisabled',
+		'value'
+	]}
+
+
+	setDisabledState(bool) {
+		bool ? this.valueEl.setAttribute('disabled', true) : this.valueEl.removeAttribute('disabled')
+	}
+
+	setHideValueState(bool) {
+		hidden ? this.classList.add('hidden') : this.classList.remove('hidden')
+	}
+}
+
+customElements.define(ReusableInput.nameIs, ReusableInput);
